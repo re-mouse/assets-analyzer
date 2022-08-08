d2c302382adc9c40f69084a7703badd024fe76d2
Niyaz Kashafutdinov <n.kashafutdinov@balaganov.rocks>
2022-08-09 00:24:06 +0300

    - feat: utils

---
+++ Editor/Layout.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DependenciesHunter
+{
+    public static class Layout {
+        public static void Horizontal(Action block, int width = 0, int height = 0)
+        {
+            List<GUILayoutOption> options = new List<GUILayoutOption>();
+            if (width != 0)
+                options.Add(GUILayout.);
+            GUILayout.BeginHorizontal();
+            
+            block();
+            
+            GUILayout.EndHorizontal();
+        }
+
+        public static void Vertical(Action block) {
+            GUILayout.BeginVertical();
+            
+            block();
+            
+            GUILayout.EndVertical();
+        }
+    }
+}
